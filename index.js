39cb222fd6ee34ad6f97ef266c64d6c6a8d57970
Nuogz <zheung@vip.qq.com>
2021-08-17 17:22:12 +0800

    3.1.0 ES MODULE! ES2021! restruct all log4js configuration, more efficient support log-update! support log empty-line

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import formatLog from './lib/formatLog.js';
 import configureFile from './lib/FileAppender.js';
 import configureConsole from './lib/ConsoleAppender.js';
 
+import symbolLogUpdate from './lib/LogUpdateSymbol.js';
+
 
 /**
  * 接口配置
@@ -49,7 +51,7 @@ export let hasFirstInited = false;
  *   - error: 错误
  *   - fatal: 致命
  *   - mark: 标记
- * @version 3.0.0-2021.08.17.01
+ * @version 3.1.0-2021.08.17.02
  * @class
  * @requires chalk(4)
  * @requires log-update(4)
@@ -118,14 +120,14 @@ const Hades = class Hades {
 		configure.appenders[keyAppenderFile] = {
 			type: { configure: configureFile },
 			path: join(pathSave, name + '.log'),
-			maxLogSize: 1024 * 1024 * 20, eol: '\n',
-			handle: event => formatLog(event, isHightlight)[0],
+			isHightlight, maxLogSize: 1024 * 1024 * 20, eol: '\n',
+			handle: (event, isHightlight) => formatLog(event, isHightlight)[0],
 		};
 		configure.appenders[keyAppenderFileStack] = {
 			type: { configure: configureFile },
 			path: join(pathSave, name + '.stack.log'),
-			maxLogSize: 1024 * 1024 * 20, eol: '\n',
-			handle: event => formatLog(event, isHightlight)[1],
+			isHightlight, maxLogSize: 1024 * 1024 * 20, eol: '\n',
+			handle: (event, isHightlight) => formatLog(event, isHightlight)[1],
 		};
 
 
@@ -145,7 +147,8 @@ const Hades = class Hades {
 
 		if(this.isLogInited) {
 			if(pathSave) {
-				this.info('日志', '加载', '✔ ', `日志路径{${pathSave}}`);
+				this.info();
+				// this.info('日志', '加载', '✔ ', `日志路径{${pathSave}}`);
 			}
 			else {
 				this.info('日志', '加载', '✔ ');
@@ -165,6 +168,11 @@ const Hades = class Hades {
 	}
 
 	/**
+	 * 控制台输入更新标记
+	 */
+	get U() { return symbolLogUpdate; }
+
+	/**
 	 * 跟踪（trace）
 	 * - 用于`可能大量`显示的底层的数据跟踪
 	 * - 如循环中的循环量的`i`等
@@ -174,7 +182,8 @@ const Hades = class Hades {
 	 * @param {any} where 在做什么
 	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
 	  */
-	trace(where, what, ...infos) { this.logger.trace(where, what, ...infos); }
+	// eslint-disable-next-line no-unused-vars
+	trace(where, what, ...infos) { this.logger.trace(...arguments); }
 	/**
 	  * 调试（debug）
 	  * - 用于`频率不高`的`经历过计算`的数据跟踪
@@ -184,7 +193,8 @@ const Hades = class Hades {
 	  * @param {any} where 在做什么
 	  * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
 	  */
-	debug(where, what, ...infos) { this.logger.debug(where, what, ...infos); }
+	// eslint-disable-next-line no-unused-vars
+	debug(where, what, ...infos) { this.logger.debug(...arguments); }
 	/**
 	 * 信息（info）
 	 * - 用于常规的业务摘要或`非正常但非异常`的数据记录
@@ -193,7 +203,8 @@ const Hades = class Hades {
 	 * @param {any} where 在做什么
 	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
 	 */
-	info(where, what, ...infos) { this.logger.info(where, what, ...infos); }
+	// eslint-disable-next-line no-unused-vars
+	info(where, what, ...infos) { this.logger.info(...arguments); }
 	/**
 	 * 警告（warn）
 	 * - 用于`有可能`导致异常的运行记录
@@ -203,7 +214,8 @@ const Hades = class Hades {
 	 * @param {any} where 在做什么
 	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
 	 */
-	warn(where, what, ...infos) { this.logger.warn(where, what, ...infos); }
+	// eslint-disable-next-line no-unused-vars
+	warn(where, what, ...infos) { this.logger.warn(...arguments); }
 	/**
 	 * 错误（error）
 	 * - 用于`异常`逻辑、`错误`数据的运行记录
@@ -213,7 +225,8 @@ const Hades = class Hades {
 	 * @param {any} where 在做什么
 	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
 	 */
-	error(where, what, ...infos) { this.logger.error(where, what, ...infos); }
+	// eslint-disable-next-line no-unused-vars
+	error(where, what, ...infos) { this.logger.error(...arguments); }
 	/**
 	 * 致命（fatal）
 	 * - 用于可能导致`程序退出`的严重的运行记录
@@ -223,7 +236,8 @@ const Hades = class Hades {
 	 * @param {any} where 在做什么
 	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
 	 */
-	fatal(where, what, ...infos) { this.logger.fatal(where, what, ...infos); }
+	// eslint-disable-next-line no-unused-vars
+	fatal(where, what, ...infos) { this.logger.fatal(...arguments); }
 	/**
 	 * 标记（mark）
 	 * - 最高级的日记记录，通常用于无关运行情况的必要的说明
@@ -234,7 +248,88 @@ const Hades = class Hades {
 	 * @param {any} where 在做什么
 	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
 	 */
-	mark(where, what, ...infos) { this.logger.mark(where, what, ...infos); }
+	// eslint-disable-next-line no-unused-vars
+	mark(where, what, ...infos) { this.logger.mark(...arguments); }
+
+
+	/**
+	 * 跟踪（trace、行内更新）
+	 * - 用于`可能大量`显示的底层的数据跟踪
+	 * - 如循环中的循环量的`i`等
+	 * - 不应在生产环境中使用，也不应出现在提交的代码中，通常调试后立即删除
+	 * - 颜色：blue
+	 * @param {any} where 在哪里发生
+	 * @param {any} where 在做什么
+	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
+	  */
+	// eslint-disable-next-line no-unused-vars
+	traceU(where, what, ...infos) { this.logger.trace(symbolLogUpdate, ...arguments); }
+	/**
+	  * 调试（debug、行内更新）
+	  * - 用于`频率不高`的`经历过计算`的数据跟踪
+	  * - 如某个函数的结果、不重要的心跳回复等
+	  * - 颜色：cyan
+	  * @param {any} where 在哪里发生
+	  * @param {any} where 在做什么
+	  * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
+	  */
+	// eslint-disable-next-line no-unused-vars
+	debugU(where, what, ...infos) { this.logger.debug(symbolLogUpdate, ...arguments); }
+	/**
+	 * 信息（info、行内更新）
+	 * - 用于常规的业务摘要或`非正常但非异常`的数据记录
+	 * - 颜色：green
+	 * @param {any} where 在哪里发生
+	 * @param {any} where 在做什么
+	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
+	 */
+	// eslint-disable-next-line no-unused-vars
+	infoU(where, what, ...infos) { this.logger.info(symbolLogUpdate, ...arguments); }
+	/**
+	 * 警告（warn、行内更新）
+	 * - 用于`有可能`导致异常的运行记录
+	 * - 如程序启动时，数据库测试连接超时，但程序认为可以稍后再次测试
+	 * - 颜色：yellow
+	 * @param {any} where 在哪里发生
+	 * @param {any} where 在做什么
+	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
+	 */
+	// eslint-disable-next-line no-unused-vars
+	warnU(where, what, ...infos) { this.logger.warn(symbolLogUpdate, ...arguments); }
+	/**
+	 * 错误（error、行内更新）
+	 * - 用于`异常`逻辑、`错误`数据的运行记录
+	 * - 如业务运行时，数据库运行失败。必要字段为空导致业务中止等
+	 * - 颜色：red
+	 * @param {any} where 在哪里发生
+	 * @param {any} where 在做什么
+	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
+	 */
+	// eslint-disable-next-line no-unused-vars
+	errorU(where, what, ...infos) { this.logger.error(symbolLogUpdate, ...arguments); }
+	/**
+	 * 致命（fatal、行内更新）
+	 * - 用于可能导致`程序退出`的严重的运行记录
+	 * - 如未捕获的异常、意外的文件读写
+	 * - 颜色：magenta
+	 * @param {any} where 在哪里发生
+	 * @param {any} where 在做什么
+	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
+	 */
+	// eslint-disable-next-line no-unused-vars
+	fatalU(where, what, ...infos) { this.logger.fatal(symbolLogUpdate, ...arguments); }
+	/**
+	 * 标记（mark、行内更新）
+	 * - 最高级的日记记录，通常用于无关运行情况的必要的说明
+	 * - 除非关闭日志，否则都会输出日志
+	 * - 如版权说明、注意事项等
+	 * - 颜色：grey
+	 * @param {any} where 在哪里发生
+	 * @param {any} where 在做什么
+	 * @param {any[]} infos 日志内容。第一个内容不换行，第二个内容开始换行并缩进
+	 */
+	// eslint-disable-next-line no-unused-vars
+	markU(where, what, ...infos) { this.logger.mark(symbolLogUpdate, ...arguments); }
 };
 
 
